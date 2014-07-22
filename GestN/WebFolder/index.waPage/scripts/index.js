19dc1109959b3b69568989acbcf16a8ed9c8e9c9
Jeddi Fouad <jeddi.fouad@gmail.com>
2014-07-22 18:27:17 +0100

    Adding Container
    Good To Go

---
+++ GestN/WebFolder/index.waPage/scripts/index.js
@@ -0,0 +1,18 @@
+
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var documentEvent = {};	// @document
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	documentEvent.onLoad = function documentEvent_onLoad (event)// @startlock
+	{// @endlock
+		ds.Etudiant.all();
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("document", "onLoad", documentEvent.onLoad, "WAF");
+// @endregion
+};// @endlock
